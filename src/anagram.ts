73fa9c897ec3055046a07e30824bd340cfcea327
Erin Bergman <erin.e.bergman@gmail.com>
2017-04-20 10:32:47 -0400

    Change the algorithm to check if the two strings just have the exact number of characters

--- src/anagram.ts
+++ src/anagram.ts
@@ -6,9 +6,10 @@
  * Given two strings, determine if the are anagrams of each other
  */
 
+// Just check if they have the exact same characters
+
 export function areAnagrams(string1: string, string2: string): boolean {
-    for (const permutation of permutations(string1)) {
-        if (permutation === string2) return true;
-    }
-    return false;
+    string1 = string1.split('').sort().join('');
+    string2 = string2.split('').sort().join('');
+    return string1 === string2;
 }
