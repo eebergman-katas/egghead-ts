87cb26d613aeec00ff97982899980611d5b437b0
Erin Bergman <erin.e.bergman@gmail.com>
2017-04-20 10:30:01 -0400

    Add rules

--- src/anagram.ts
+++ src/anagram.ts
@@ -1,5 +1,11 @@
 // Algorithm to determine if two strings are an anagram
 
+/**
+ * A word is an anagram of another, if you can rearrange its characters to produce the second word.
+ *  
+ * Given two strings, determine if the are anagrams of each other
+ */
+
 export function areAnagrams(string1: string, string2: string): boolean {
     for (const permutation of permutations(string1)) {
         if (permutation === string2) return true;
