4e91141fe2eff8d189845fd34bf6b3dcbc2b39f6
Erin Bergman <erin.e.bergman@gmail.com>
2017-04-20 10:26:34 -0400

    Inital commit

---
+++ src/anagram.ts
@@ -0,0 +1,8 @@
+// Algorithm to determine if two strings are an anagram
+
+export function areAnagrams(string1: string, string2: string): boolean {
+    for (const permutation of permutations(string1)) {
+        if (permutation === s2) return true;
+    }
+    return false;
+}
